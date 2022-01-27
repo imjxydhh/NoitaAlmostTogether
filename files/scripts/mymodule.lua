0d0a87d1fe8e59f2786b7f769a4bd332c0261b9c
jyydhh <dcbajk@vip.qq.com>
2022-01-27 22:18:36 +0800

    22.01.27: enable restricted api

--- files/scripts/mymodule.lua
+++ files/scripts/mymodule.lua
@@ -1,9 +1,19 @@
+require "mods.my_first_mod.files.scripts.test_module"
+-- ffi = require("ffi")
+
 module = {}
 
 module.test = 1
 
 function module.HelloWorld()	
 	print("hello world" .. module.test)
+	test_module.fooFun1()
+	-- ffi.cdef[[
+		-- int printf(const char* fmt, ...);
+	-- ]]
+
+	-- ffi.C.printf()
 end
+
 
 return module
---
+++ files/scripts/test_module.lua
@@ -0,0 +1,9 @@
+test_module = {}
+
+test_module.fooVal = 1
+
+function test_module.fooFun1()
+	print("This is fooFun 1")
+end
+
+return test_module
